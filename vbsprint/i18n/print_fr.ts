9c6bf993ddfdd462c90d2181346ecd4f04fcaebd
Sandro Mani <manisandro@gmail.com>
2016-06-16 19:41:16 +0200

    Add french translation

---
+++ vbsprint/i18n/print_fr.ts
@@ -0,0 +1,306 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr">
+<context>
+    <name>CartoucheDialog</name>
+    <message>
+        <location filename="../ui/ui_cartouchedialog.py" line="143"/>
+        <source>Map Cartouche</source>
+        <translation>Cartouche</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_cartouchedialog.py" line="144"/>
+        <source>Exercise</source>
+        <translation>Exercice</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_cartouchedialog.py" line="145"/>
+        <source>Course name</source>
+        <translation>Désignation du cours</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_cartouchedialog.py" line="146"/>
+        <source>Exercise name</source>
+        <translation>Désignation de l&apos;exercice</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_cartouchedialog.py" line="147"/>
+        <source>Document name</source>
+        <translation>Désignation du document</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_cartouchedialog.py" line="155"/>
+        <source>CLASSIFICATION</source>
+        <translation>CLASSIFICATION</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_cartouchedialog.py" line="156"/>
+        <source>RESTRICTED</source>
+        <translation>LIMITÉ</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_cartouchedialog.py" line="157"/>
+        <source>CONFIDENTIAL</source>
+        <translation>CONFIDENTIEL</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_cartouchedialog.py" line="158"/>
+        <source>SECRET</source>
+        <translation>SECRET</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_cartouchedialog.py" line="152"/>
+        <source>Exercise organisation</source>
+        <translation>Organisation de l&apos;exercice</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_cartouchedialog.py" line="153"/>
+        <source>Annex name</source>
+        <translation>Désignation des annexes</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_cartouchedialog.py" line="154"/>
+        <source>Title of cartouche</source>
+        <translation>Description de la cartouche</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_cartouchedialog.py" line="159"/>
+        <source>Place, date</source>
+        <translation>Lieu, date</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_cartouchedialog.py" line="160"/>
+        <source>&quot;CODE NAME&quot;</source>
+        <translation>&quot;NOM DE DOUVERTURE&quot;</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_cartouchedialog.py" line="161"/>
+        <source>Unit</source>
+        <translation>Désignation de la troupe</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_cartouchedialog.py" line="162"/>
+        <source>Scale information</source>
+        <translation>Indication de l&apos;échelle</translation>
+    </message>
+</context>
+<context>
+    <name>PrintDialog</name>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="280"/>
+        <source>Print</source>
+        <translation>Imprimer</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="281"/>
+        <source>Border:</source>
+        <translation>Marge:</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="282"/>
+        <source>Extent:</source>
+        <translation>Étendue:</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="283"/>
+        <source> mm</source>
+        <translation> mm</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="284"/>
+        <source>X Max.:</source>
+        <translation>X Max.:</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="285"/>
+        <source>X Min.:</source>
+        <translation>X Min.:</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="286"/>
+        <source>Y Max.:</source>
+        <translation>Y Max.:</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="287"/>
+        <source>Y Min.:</source>
+        <translation>Y Min.:</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="288"/>
+        <source>Grid</source>
+        <translation>Grille</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="289"/>
+        <source>Coordinate system:</source>
+        <translation>Système de coordonnées:</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="290"/>
+        <source>Interval X:</source>
+        <translation>Intervalle X:</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="291"/>
+        <source>Coordinate labels</source>
+        <translation>Afficher les coordonnées</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="292"/>
+        <source>Interval Y:</source>
+        <translation>Intervalle Y:</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="293"/>
+        <source>Layout:</source>
+        <translation>Modèle:</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="294"/>
+        <source>Title:</source>
+        <translation>Titre:</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="295"/>
+        <source>Scalebar</source>
+        <translation>Barre d&apos;échelle</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="296"/>
+        <source>Legend</source>
+        <translation>Légende</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="297"/>
+        <source>Edit map cartouche</source>
+        <translation>Éditer Cartouche</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="298"/>
+        <source>File format:</source>
+        <translation>Format de fichier:</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="299"/>
+        <source>Map cartouche</source>
+        <translation>Cartouche</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="300"/>
+        <source>Scale:</source>
+        <translation>Échelle:</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="301"/>
+        <source>Only meter based coordinate systems are supported in variable extent mode.</source>
+        <translation>Seulement systèmes de coordonnées métriques peuvent être utilisées en mode de taille variable.</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="302"/>
+        <source>Configure</source>
+        <translation>Configurer</translation>
+    </message>
+</context>
+<context>
+    <name>PrintPlugin</name>
+    <message>
+        <location filename="../PrintPlugin.py" line="52"/>
+        <source> Print</source>
+        <translation>Imprimer</translation>
+    </message>
+</context>
+<context>
+    <name>PrintTool</name>
+    <message>
+        <location filename="../PrintTool.py" line="39"/>
+        <source>Export</source>
+        <translation>Exportér</translation>
+    </message>
+    <message>
+        <location filename="../PrintTool.py" line="40"/>
+        <source>Print</source>
+        <translation>Imprimer</translation>
+    </message>
+    <message>
+        <location filename="../PrintTool.py" line="41"/>
+        <source>Advanced</source>
+        <translation>Avancée</translation>
+    </message>
+    <message>
+        <location filename="../PrintTool.py" line="42"/>
+        <source>PDF Document (*.pdf);;</source>
+        <translation>Document PDF (*.pdf);;</translation>
+    </message>
+    <message>
+        <location filename="../PrintTool.py" line="43"/>
+        <source>JPG Image (*.jpg);;</source>
+        <translation>Image JPG (*.jpg);;</translation>
+    </message>
+    <message>
+        <location filename="../PrintTool.py" line="44"/>
+        <source>BMP Image (*.bmp);;</source>
+        <translation>Image BMP (*.jpg);;</translation>
+    </message>
+    <message>
+        <location filename="../PrintTool.py" line="45"/>
+        <source>PNG Image (*.png);;</source>
+        <translation>Image PNG (*.jpg);;</translation>
+    </message>
+    <message>
+        <location filename="../PrintTool.py" line="373"/>
+        <source>Hidden</source>
+        <translation>Caché</translation>
+    </message>
+    <message>
+        <location filename="../PrintTool.py" line="373"/>
+        <source>Visible without layer legend</source>
+        <translation>Visible sans légende du couche</translation>
+    </message>
+    <message>
+        <location filename="../PrintTool.py" line="373"/>
+        <source>Visible with layer legend</source>
+        <translation>Visible avec légende du couche</translation>
+    </message>
+    <message>
+        <location filename="../PrintTool.py" line="383"/>
+        <source>Configure legend</source>
+        <translation>Configurer la légende</translation>
+    </message>
+    <message>
+        <location filename="../PrintTool.py" line="385"/>
+        <source>Select layers to display in legend:</source>
+        <translation>Sélectionnez les couches à afficher dans la légende:</translation>
+    </message>
+    <message>
+        <location filename="../PrintTool.py" line="489"/>
+        <source>Invalid composer</source>
+        <translation>Composeur invalide</translation>
+    </message>
+    <message>
+        <location filename="../PrintTool.py" line="489"/>
+        <source>The composer must have exactly one map item.</source>
+        <translation>Le composeur doit avoir exactement un élément de carte.</translation>
+    </message>
+    <message>
+        <location filename="../PrintTool.py" line="544"/>
+        <source>Paper size: %.2f cm x %.2f cm</source>
+        <translation>Taille de papier: %.2f cm x %.2f cm</translation>
+    </message>
+    <message>
+        <location filename="../PrintTool.py" line="585"/>
+        <source>Print Composition</source>
+        <translation>Imprimer composition</translation>
+    </message>
+    <message>
+        <location filename="../PrintTool.py" line="612"/>
+        <source>Print Failed</source>
+        <translation>Impossible de imprimer</translation>
+    </message>
+    <message>
+        <location filename="../PrintTool.py" line="612"/>
+        <source>Failed to print the composition.</source>
+        <translation>Impossible de imprimer la composition.</translation>
+    </message>
+</context>
+</TS>
