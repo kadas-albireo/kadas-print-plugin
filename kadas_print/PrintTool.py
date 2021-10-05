4d4f4a126f594ff214f64ababbff0a35c322a969
Sandro Mani <manisandro@gmail.com>
2021-10-05 10:38:18 +0200

    Default to legend with titles + image

--- kadas_print/PrintTool.py
+++ kadas_print/PrintTool.py
@@ -387,7 +387,7 @@ class PrintTool(KadasMapToolSelectRect):
                 combo.setCurrentIndex(
                     2 if prevLayers[layerNode.layerId()] is True else 1)
             else:
-                combo.setCurrentIndex(0)
+                combo.setCurrentIndex(2)
             layersList.setCellWidget(row, 1, combo)
         legendDialog = QDialog(self.dialog)
         if execDialog:
